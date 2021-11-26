52c36b9b9cc30427a98dc3790e9ffbeae39923b5
sophiakibaker <72014836+sophiakibaker@users.noreply.github.com>
2021-11-26 18:31:52 +0000

    Oneshot Script

--- 2_stacking_and_plotting.py
+++ 2_stacking_and_plotting.py
@@ -66,11 +66,9 @@ datef=str(sys.argv[11])
 
 #----------------------- Processing SCRIPTS --------------------------
 
-Filters = ['jgf1']#,'jgf1']#'tff4', 'tff5'] #'jgf3', 'jgf2','tff1', 'tff2', 'tff3', 
-Models = ['prem']#,'ak135']
-Noise = ['Alistair']#,'Sanne', 'Sophia'] 
-
-
+Filters = ['jgf1', 'jgf2', 'jgf3', 'tff1', 'tff2', 'tff3', 'tff4', 'tff5']
+Models = ['prem','ak135']
+Noise = ['Alistair','Sanne','Sophia'] 
 
 
 for Model in Models:
@@ -81,21 +79,21 @@ for Model in Models:
 
                 for noisefilter in Noise:
 
-                        # RFsPerStation.rfsperstation(Data, noisefilter, filt)
+                        RFsPerStation.rfsperstation(Data, noisefilter, filt)
                         print('RFsPerStation')
 
                         ### Migration ###
-                        # calculate_pierce_points.calculate_pierce_points(Data, noisefilter, Model, '410', 'P410s', filt)
+                        calculate_pierce_points.calculate_pierce_points(Data, noisefilter, Model, '410', 'P410s', filt)
                         print('calculate_pierce_points_410')
                         
-                        # calculate_pierce_points.calculate_pierce_points(Data, noisefilter, Model, '660', 'P660s', filt)
+                        calculate_pierce_points.calculate_pierce_points(Data, noisefilter, Model, '660', 'P660s', filt)
                         print('calculate_pierce_points_660')
                          # Description: Calculate converted phase pierce points at discontinuity depths  
                                 # Inputs: Depth of piercepoints, Phase, filter band, 1D velocity model  
                                 # Outputs: Adds PP for given phase and discont depth to each Pickle file, prints to file PP_'DEPTH'km_'PHASE'_'FILTER'.txt'  
                          # Usage: python3 calculate_pierce_points.py 410 P410s jgf1  
 
-                        # convert_to_depth_obspy.convert_to_depth(Data,noisefilter, Model,filt)
+                        convert_to_depth_obspy.convert_to_depth(Data,noisefilter, Model,filt)
                         print('convert_to_depth')
                          # Description: Convert RF from time to depth using 1D model
                          # Inputs: Filter band, 1D velocity model  
@@ -112,69 +110,71 @@ for Model in Models:
                                 # Outputs: BAZ stack plot  
                                 # Usage: python3 epicentral_distance_stack.py Data 5 410 False -179 179 -89 89 30 90 jgf1  
 
-                                # depth_stack.depth_stack(Data, noisefilter, Model, lonmin, lonmax, latmin, latmax, filt) 
+                                depth_stack.depth_stack(Data, noisefilter, Model, lonmin, lonmax, latmin, latmax, filt) 
                                 print('depth_stack')
                                 # # Description: Stacks all the RFs within the bounds for the depth stated producing one trace.  
                                 # # Inputs: conversion, lon/lat box, filter band  
                                 # # Outputs: Depth stack pickle file and pdf/png  
                                 # # Usage: python3 depth_stack.py prem -179 179 -89 89 jgf1  
 
-                                # slowness_stack.slowness_stack(Data, noisefilter, lonmin, lonmax, latmin, latmax, filt)
+                                slowness_stack.slowness_stack(Data, noisefilter, lonmin, lonmax, latmin, latmax, filt)
                                 print('slowness_stack')
                                 # Description: Plot of slowness against time, using a specfic epicentral reference distance.  
                                 # Inputs: lon/lat box, filter band  
                                 # Outputs: Slowness stack pickle file and pdf/png  
                                 # Usage: python3 slowness_stack.py -179 179 -89 89 jgf1  
 
-                                # num_rfs = slowness_stack.slowness_stack(Data, noisefilter, lonmin, lonmax, latmin, latmax, filt)
+                                num_rfs = slowness_stack.slowness_stack(Data, noisefilter, lonmin, lonmax, latmin, latmax, filt)
 
-                                # depth_slowness_stack.depth_slowness(Data, Model, lonmin, lonmax, latmin, latmax, noisefilter, filt, num_rfs)  
+                                depth_slowness_stack.depth_slowness(Data, Model, lonmin, lonmax, latmin, latmax, noisefilter, filt, num_rfs)  
                                 # [OPTIONAL] Description: Combines previously calculated depth and slowness stack in one figure.  
                                 # (Data,conv,lonmin,lonmax,latmin,latmax,noisefilter,rffilter,num_rfs):
                                 # Inputs: Depth and slowness stack pickle files  
                                 # Outputs: Combined depth and slowness stack image pdf/png  
                                 # Usage: python3 depth_slowness_stack.py prem -179 179 -89 89 jgf1 339  
 
-                                # stack_CCP.stack_CCP(Data, noisefilter, 'CCP_Global', Model, lonmin, lonmax, latmin, latmax, filt, 2.0, True) 
+                                stack_CCP.stack_CCP(Data, noisefilter, 'CCP_Global', Model, lonmin, lonmax, latmin, latmax, filt, 2.0, True) 
                                 # # Description: wrapper for the function contained within common_conversion_point_stack.py  
                                 # # Inputs: noisefilter, conversion, lon/lat box, filter band, smoothing factor, newstack  
                                 # # Outputs: Common conversion point stack volume (PICKLE)  
                                 # # Usage: python3 stack_CCP.py CCP_Global prem -179.0 179.0 -89.0 89.0 jgf1 2.0 True  
 
                                 # ### Plotting ###
-                                # plot_map_pierce_points.plot_pierce_points(Data, noisefilter, 410, 'P410s', filt)
-                                # plot_map_pierce_points.plot_pierce_points(Data, noisefilter, 660, 'P660s', filt)
+                                plot_map_pierce_points.plot_pierce_points(Data, noisefilter, 410, 'P410s', filt)
+                                plot_map_pierce_points.plot_pierce_points(Data, noisefilter, 660, 'P660s', filt)
 
-                                # plot_map_stations_pierce_points.plot_pierce_points(Data, noisefilter, filt)
+                                plot_map_stations_pierce_points.plot_pierce_points(Data, noisefilter, filt)
                                 # # Description: Plots discontinuity depth pierce points  
                                 # # Inputs: discontinuity depth, converted phase  
                                 # # Outputs: matplotlib plot  
                                 # # Usage: python3 plot_map_pierce_points.py 410 P410s jgf1   
 
-                                ccpstyles = ['COV', 'TOPO', 'THICK', 'GETTHICK', 'MOVEOUT', '3DTOPO'] 
+                                ccpstyles = ['COV', 'TOPO', 'THICK', 'GETTHICK', 'MOVEOUT'] 
                                 slices = ['NS', 'EW']
                                 latorlon = [118, 6]
 
-                                # for ccpstyle in ccpstyles:
-                                #         plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, ccpstyle, 410)
-                                #         plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, ccpstyle, 660)
+                                for ccpstyle in ccpstyles:
+                                        plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, ccpstyle, 410)
+                                        plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, ccpstyle, 660)
                                 
                                 plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, '3DMTZ', 410)
                                 
-                                quit()
-                                # for i in range(len(slices)):
-                                #         if slices[i] == 'NS':
-                                #                 latorlon = [116, 116.5, 117, 119]
-                                #         else:
-                                #                 latorlon = [6, 4.8]
 
-                                #         for ll in latorlon:
-                                #                 plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, slices[i], [ll, 1.5])
+                                for i in range(len(slices)):
+                                        if slices[i] == 'NS':
+                                                latorlon = [116, 116.5, 117, 119]
+                                        else:
+                                                latorlon = [6, 4.8]
+
+                                        for ll in latorlon:
+                                                plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global',Model, filt, 2.0, 2.0, slices[i], [ll, 1.5])
 
 
                                 crosssection = [116, 6.5, 118.75, 4, 1.5, 60] #[116, 8, 120, 3, .75, 60] 
 
-                                # plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global', Model, filt, 2.0, 2.0, 'XC', crosssection)
+                                plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global', Model, filt, 2.0, 2.0, 'XC', crosssection)
+                                
+                                plot_CCP.plot_CCP(Data, noisefilter, 'CCP_Global', Model, filt, 2.0, 2.0, '3DTOPO', 410)
 
                                 # Description: Wrapper for the function contained within CCP_plottingroutines.py  
                                 # Inputs: noisefilter, conversion, filter band, smoothing factor, mincoverage, plot_type, plot_params (doesn't just have to be depth)
@@ -182,4 +182,4 @@ for Model in Models:
                                 # Usage: python3 plot_CCP.py CCP_Global prem jgf1 2.0 2.0 COV 410  
                                 # Usage: python3 plot_CCP.py CCP_Global prem jgf1 2.0 2.0 NS lonorlatseed amp
 
-print('Stacking and plotting for '+str(Filters+Models+Noise)+' took' + str(time.time()-starttime) + 'seconds.')
+  print('Stacking and plotting for '+str(Filters+Models+Noise)+' took' + str(time.time()-starttime) + 'seconds.')

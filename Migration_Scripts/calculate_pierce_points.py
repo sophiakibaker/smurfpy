7bb203770316b4717db8774091cc11416bb7de17
sophiakibaker <72014836+sophiakibaker@users.noreply.github.com>
2021-11-26 17:51:17 +0000

    Migration Scripts (Sophia's Version)
    Adapted for use in "Oneshot Scripts", which call modified versions of original SMURFPy as functions, allowing easy, automated looping through filters and RF selection criteria, and minimal user input.

--- Migration_Scripts/calculate_pierce_points.py
+++ Migration_Scripts/calculate_pierce_points.py
@@ -4,9 +4,9 @@ This script computes predicted pierce points for user defined phases based on Ta
 It loops through all the data, checking whether the pierce points have been calculated,
 and if not working them out, writing the data to sperate file and the PICKLE file itself.
 
-sys.argv[1] = Depth of piercepoints
-sys.argv[2] = Phase
-sys.argv[3] = Filter
+depth = Depth of piercepoints
+phase = Phase
+Filt = Filter
 
 eg. Piercepoints_Calc.py 410 P410s jgf1
 '''
@@ -16,135 +16,137 @@ from obspy import read
 import os, sys, glob
 import subprocess
 
-# Command line help
-if len(sys.argv) != 4 or str(sys.argv[1]).lower() == 'help':
-    print('\n')
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    print(sys.argv[0])
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    print('Description:           Calculate converted phase pierce points at discontinuity depths')
-    print('Inputs:                Depth of piercepoints, Phase, filter band, 1D velocity model')
-    print('Outputs:               Adds PP for given phase and discont depth to each Pickle file, prints to file')
-    print('                       PP_DEPTHkm_PHASE_FILTER.txt\n')
-    print('Usage:                 python3 calculate_pierce_points.py depth phase filter')
-    print('Format [1]:            depth (km) [int]')
-    print('Format [2]:            seismic phase [str]')
-    print('Options [3]:           jgf1, jgf2, jgf3, tff1, tff2, tff3, tff4 or tff5 [str]')
-    print('Recommended:           python3 calculate_pierce_points.py 410 P410s jgf1')
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    print('\n')
-    sys.exit()
-    
-sys.path.insert(0, 'Path/to/smurfpy/Tools/TauP-2.5.0/bin')
+# # Command line help
+# if len(sys.argv) != 4 or str(depth).lower() == 'help':
+#     print('\n')
+#     print('-----------------------------------------------------------------------------------------------------------------------')
+#     print(sys.argv[0])
+#     print('-----------------------------------------------------------------------------------------------------------------------')
+#     print('Description:           Calculate converted phase pierce points at discontinuity depths')
+#     print('Inputs:                Depth of piercepoints, Phase, filter band, 1D velocity model')
+#     print('Outputs:               Adds PP for given phase and discont depth to each Pickle file, prints to file')
+#     print('                       PP_DEPTHkm_PHASE_FILTER.txt\n')
+#     print('Usage:                 python3 calculate_pierce_points.py depth phase filter')
+#     print('Format [1]:            depth (km) [int]')
+#     print('Format [2]:            seismic phase [str]')
+#     print('Options [3]:           jgf1, jgf2, jgf3, tff1, tff2, tff3, tff4 or tff5 [str]')
+#     print('Recommended:           python3 calculate_pierce_points.py 410 P410s jgf1')
+#     print('-----------------------------------------------------------------------------------------------------------------------')
+#     print('\n')
+#     sys.exit()
 
-PREM=True
-ak135=False
-if PREM:
-    mod='prem'
-elif ak135:
-    mod='ak135'
+def calculate_pierce_points(Data, noise, Model, depth, phase, Filt):
 
+    mod = Model
 
+    # PREM=True
+    # ak135=False
+    # if PREM:
+    #     mod='prem'
+    # elif ak135:
+    #     mod='ak135'
 
-# Make and open a data file in the Piercepoints directory for this phase
-# and depth ('w' = writable)
-PP = open('PP_' + str(sys.argv[1]) + 'km_' + str(sys.argv[2]) + '_' + str(sys.argv[3]) + '.txt', 'w')
+    directory = Data+"_Results/"
 
-# Loop through stations
-direc = '../Data'
-stadirs = glob.glob(direc + '/*')
-for stadir in stadirs:
-    stalist = []
-    if os.path.isfile(stadir + '/selected_RFs_'+str(sys.argv[3])+'.dat'):
-        with open(stadir + '/selected_RFs_'+str(sys.argv[3])+'.dat') as a:
-            starfs = a.read().splitlines()
-            for line in starfs:
-                stalist.append(line)
+    # Make and open a data file in the Piercepoints directory for this phase
+    # and depth ('w' = writable)
+    PP = open(directory+'PP_' + str(depth) + 'km_' + str(phase) + '_' + noise + str(Filt) + '.txt', 'w')
 
-    # Loop through events
-    for s in range(len(stalist)):
-        seis = read(stalist[s], format='PICKLE')
-        print (stalist[s])
+    # quit()
 
-        # Note stats of stream
-        EVLA = seis[0].stats['evla']
-        EVLO = seis[0].stats['evlo']
-        EVDP = seis[0].stats['evdp']
-        STLA = seis[0].stats['stla']
-        STLO = seis[0].stats['stlo']
+    # Loop through stations
+    direc = Data
+    stadirs = glob.glob(direc + '/*')
+    for stadir in stadirs:
+        stalist = []
+        if os.path.isfile(stadir + '/selected_RFs_'+ noise+str(Filt)+'.dat'):
+            with open(stadir + '/selected_RFs_'+ noise+str(Filt)+'.dat') as a:
+                starfs = a.read().splitlines()
+                for line in starfs:
+                    stalist.append(line)
 
-        # Check if there's a piercepoints dictionary
-        if not hasattr(seis[0].stats, 'piercepoints'):
+        # Loop through events
+        for s in range(len(stalist)):
+            seis = read(stalist[s], format='PICKLE')
+            print (stalist[s])
 
-            # Start a piercepoints dictionary
-            seis[0].stats.piercepoints = dict()
+            # Note stats of stream
+            EVLA = seis[0].stats['evla']
+            EVLO = seis[0].stats['evlo']
+            EVDP = seis[0].stats['evdp']
+            STLA = seis[0].stats['stla']
+            STLO = seis[0].stats['stlo']
 
-        # Set up a variable to check whether the process needs to be done
-        runthis = False
+            # Check if there's a piercepoints dictionary
+            if not hasattr(seis[0].stats, 'piercepoints'):
 
-        # Check if the dictionary has this phase in
-        if not str(sys.argv[2]) in seis[0].stats.piercepoints.keys():
-            runthis = True
+                # Start a piercepoints dictionary
+                seis[0].stats.piercepoints = dict()
 
-        # Check whether the dictionary with this phase has this depth in
-        if str(sys.argv[2]) in seis[0].stats.piercepoints.keys():
-            if not str(sys.argv[1]) in seis[0].stats.piercepoints[sys.argv[2]].keys():
+            # Set up a variable to check whether the process needs to be done
+            runthis = False
+
+            # Check if the dictionary has this phase in
+            if not str(phase) in seis[0].stats.piercepoints.keys():
                 runthis = True
 
-        # If the file neither has the phase or depth, run the rest of the
-        # script
-        if runthis:
-            print('runthis is True')
-            # Call taup_pierce to get piercepoints
-            test = [
-                'taup_pierce -mod ' + str(mod) + ' -h ' + str(
-                    EVDP) + ' -ph ' + sys.argv[
-                        2] + ' -pierce ' + sys.argv[
-                            1] + ' -nodiscon -sta ' + str(
-                    STLA) + ' ' + str(
-                        STLO) + ' -evt ' + str(
-                            EVLA) + ' ' + str(
-                                EVLO)]
+            # Check whether the dictionary with this phase has this depth in
+            if str(phase) in seis[0].stats.piercepoints.keys():
+                if not str(depth) in seis[0].stats.piercepoints[phase].keys():
+                    runthis = True
 
-            # Run test in terminal
-            out = subprocess.check_output(
-                test,
-                shell=True,
-                universal_newlines=True)
+            # If the file neither has the phase or depth, run the rest of the
+            # script
+            if runthis:
+                print('runthis is True')
+                # Call taup_pierce to get piercepoints
+                test = [
+                    'taup_pierce -mod ' + str(mod) + ' -h ' + str(
+                        EVDP) + ' -ph ' + phase + ' -pierce ' + depth + ' -nodiscon -sta ' + str(
+                        STLA) + ' ' + str(
+                            STLO) + ' -evt ' + str(
+                                EVLA) + ' ' + str(
+                                    EVLO)]
 
-            # Split the output into lines
-            # t[0] is a description
-            # t[1] the downwards pierce and t[2] the upwards pierce (if event depth < PP depth)
-            # t[1] the upwards pierce (if event depth > PP depth)
-            t = out.split('\n')
+                # Run test in terminal
+                out = subprocess.check_output(
+                    test,
+                    shell=True,
+                    universal_newlines=True)
 
-            # Split the relevant line into strings
-            if EVDP <= float(sys.argv[1]):
-                u = t[2].split()
+                # Split the output into lines
+                # t[0] is a description
+                # t[1] the downwards pierce and t[2] the upwards pierce (if event depth < PP depth)
+                # t[1] the upwards pierce (if event depth > PP depth)
+                t = out.split('\n')
+
+                # Split the relevant line into strings
+                if EVDP <= float(depth):
+                    u = t[2].split()
+                else:
+                    u = t[1].split()
+
+                # For the string U: PP depth = u[1], lat = u[3], lon = u[4]
+
+                # Make a separate dictionary within piercepoints for each phase
+                seis[0].stats.piercepoints[phase] = dict()
+
+                # For each phase, create a dictionary for each depth, writing
+                # piercepoint depth/lat/lon info
+                seis[0].stats.piercepoints[phase][
+                    depth] = [u[1], u[3], u[4]]
+
+                # Write pierce point data to original pickle file
+                seis.write(stalist[s], format='PICKLE')
+
+                # Add line to opened output file followed by new line
+                PP.write(str(float(u[1])) + ' ' + str(float(u[3])) + ' ' + str(float(u[4])) + '\n')
+
             else:
-                u = t[1].split()
+                PP.write(seis[0].stats.piercepoints[phase][depth][0] + ' ' + \
+                seis[0].stats.piercepoints[phase][depth][1] + ' ' + \
+                seis[0].stats.piercepoints[phase][depth][2] + '\n')
 
-            # For the string U: PP depth = u[1], lat = u[3], lon = u[4]
-
-            # Make a separate dictionary within piercepoints for each phase
-            seis[0].stats.piercepoints[sys.argv[2]] = dict()
-
-            # For each phase, create a dictionary for each depth, writing
-            # piercepoint depth/lat/lon info
-            seis[0].stats.piercepoints[sys.argv[2]][
-                sys.argv[1]] = [u[1], u[3], u[4]]
-
-            # Write pierce point data to original pickle file
-            seis.write(stalist[s], format='PICKLE')
-
-            # Add line to opened output file followed by new line
-            PP.write(str(float(u[1])) + ' ' + str(float(u[3])) + ' ' + str(float(u[4])) + '\n')
-
-        else:
-            PP.write(seis[0].stats.piercepoints[sys.argv[2]][sys.argv[1]][0] + ' ' + \
-            seis[0].stats.piercepoints[sys.argv[2]][sys.argv[1]][1] + ' ' + \
-            seis[0].stats.piercepoints[sys.argv[2]][sys.argv[1]][2] + '\n')
-
-# Close the file you are writing to
-PP.close()
-print ('Finished')
+    # Close the file you are writing to
+    PP.close()
+    print ('Finished')
--- Migration_Scripts/convert_to_depth_obspy.py
+++ Migration_Scripts/convert_to_depth_obspy.py
@@ -15,155 +15,162 @@ import subprocess
 from obspy.taup import TauPyModel
 
 # Command line help
-if len(sys.argv) != 2 or str(sys.argv[1]).lower() == 'help':
-    print('\n')
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    print(sys.argv[0])
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    print('Description:           Convert RF from time to depth using 1D model (coded for Prem)')
-    print('Inputs:                Filter band, 1D velocity model')
-    print("Outputs:               Adds dictionary seis[0].conversions['<nameof1Dmodel>'] to each Pickle file\n")
-    print('Usage:                 >> python3 convert_to_depth_obspy.py filterband')
-    print('Options [1]:           jgf1, jgf2, jgf3, tff1, tff2, tff3, tff4 or tff5 [str]')
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    print('\n')
-    sys.exit()
+# if len(sys.argv) != 2 or str(Filt).lower() == 'help':
+#     print('\n')
+#     print('-----------------------------------------------------------------------------------------------------------------------')
+#     print(sys.argv[0])
+#     print('-----------------------------------------------------------------------------------------------------------------------')
+#     print('Description:           Convert RF from time to depth using 1D model (coded for Prem)')
+#     print('Inputs:                Filter band, 1D velocity model')
+#     print("Outputs:               Adds dictionary seis[0].conversions['<nameof1Dmodel>'] to each Pickle file\n")
+#     print('Usage:                 >> python3 convert_to_depth_obspy.py filterband')
+#     print('Options [1]:           jgf1, jgf2, jgf3, tff1, tff2, tff3, tff4 or tff5 [str]')
+#     print('-----------------------------------------------------------------------------------------------------------------------')
+#     print('\n')
+#     sys.exit()
 
-PREM=True
-ak135=False
-rffilter=str(sys.argv[1])
+def convert_to_depth(Data, noise, Model, Filt):
 
-if PREM:
-    mod_1D='prem'
-    taupmodel = TauPyModel(model='../Tools/MODELS/PREM_FILES/prem_added_discon_taup.npz')
-elif ak135:
-    mod_1D='ak135'
-    #obspy.taup.taup_create.build_taup_model('./ak135_added_discon_taup.tvel','./ak135_added_discon_taup.npz')
-    taupmodel = TauPyModel(model='../Tools/MODELS/ak135_FILES/ak135_added_discon_taup.npz')
+    mod_1D=Model
 
+    # if Model == 'prem':
+    #     PREM=True
+    # if Model == 'ak135':
+    #     ak135=False
+    rffilter=str(Filt)
 
-def compute_onestation(dr,filter='jgf1'):
-
-    # Loop through receiver functions in station directory
-    station_file = open(dr + '/selected_RFs_'+str(filter)+'.dat','r')
-    stalist=station_file.read().strip().split()
-
-    # Loop through list
-    for i in range(len(stalist)): 
-        print(i, stalist[i])
-        seis=read(stalist[i],format='PICKLE') # Read in receiver function
-        RFtrace = getattr(seis[0],filter)
-        if not 'depths' in RFtrace:
-          if seis[0].stats['dist']> 30.  and seis[0].stats['dist'] <90.:
-            print('converting for ', stalist[i])
-            dist=seis[0].stats['dist']                     # Set distance
-            depth= seis[0].stats['evdp'] # set depth
-            slat=float(seis[0].stats['stla'])
-            slon=float(seis[0].stats['stlo'])
-            elat= seis[0].stats['event'].origins[0]['latitude']
-            elon= seis[0].stats['event'].origins[0]['longitude']
-
-            RF=trace.Trace()
-            RF.data= RFtrace['iterativedeconvolution']      # Define iterative deconvolution as a trace
-
-   
-            # Initialize
-            refdepths=[]
-            reftimes=[]
-            reftakeoff=[]
-            refincident=[]
-            refslow=[]
-
-            
-            if PREM:
-                # Get slownesses and incident angles based on PREM using Taup in Obspy
-                phases =  ['P','Pms','P220s','P310s','P400s','P550s','P670s','P971s','P1171s']
-                phdepths = [0,24.4,220,310,400,550,670,971,1171]
-            if ak135:
-                # get slownesses and incident angles based on ak135 using Taup in Obspy
-                phases =  ['P','Pms','P210s','P410s','P660s','P1156s']
-                phdepths = [0,35,210,410,660,1156]
+    if mod_1D == 'prem':
+        taupmodel = TauPyModel(model='/Users/r03sb21/Documents/smurfpy/Tools/MODELS/PREM_FILES/prem_added_discon_taup.npz')
+    elif mod_1D == 'ak135':
+        #obspy.taup.taup_create.build_taup_model('./ak135_added_discon_taup.tvel','./ak135_added_discon_taup.npz')
+        taupmodel = TauPyModel(model='/Users/r03sb21/Documents/smurfpy/Tools/MODELS/ak135_FILES/ak135_added_discon_taup.npz')
 
 
-            for p,ph in enumerate(phases):
-                arr = taupmodel.get_travel_times(depth,dist,[ph])
-                if p ==0:
-                    Preftime=arr[0].time
-                    Prefslow=arr[0].ray_param
-                    Preftakeoff= arr[0].takeoff_angle
-                    Prefincident = arr[0].incident_angle
-                refdepths.append(phdepths[p])
-                reftimes.append(arr[0].time-Preftime)
-                reftakeoff.append(arr[0].takeoff_angle-Preftakeoff)
-                refincident.append(arr[0].incident_angle-Prefincident)
-                refslow.append((arr[0].ray_param-Prefslow)*np.pi/180.)
-            depthspace=np.linspace(-100,1200,1300)
-            # Fit polynomials
-            takeoff_poly=np.polyfit(refdepths[1:],reftakeoff[1:],3)
-            takeoff=np.poly1d(takeoff_poly)
-            slow_poly=np.polyfit(refdepths,refslow,4)
-            slowness=np.poly1d(slow_poly)
-            time_poly=np.polyfit(refdepths,reftimes,4)
-            timepl=np.poly1d(time_poly)
+    def compute_onestation(dr,filter='Filt'):
 
-            # Save solutions to dictionary
-            if not hasattr(seis[0],'conversions'):
-                seis[0].conversions=dict()
-            seis[0].conversions[str(mod_1D)]=dict()
-            seis[0].conversions[str(mod_1D)]['depths']=depthspace
-            seis[0].conversions[str(mod_1D)]['takeoff']=takeoff(depthspace)
-            seis[0].conversions[str(mod_1D)]['slowness']=slowness(depthspace)
-            seis[0].conversions[str(mod_1D)]['time']=timepl(depthspace)
+        # Loop through receiver functions in station directory
+        if os.path.isfile(dr + '/selected_RFs_'+noise+str(filter)+'.dat'):
+            station_file = open(dr + '/selected_RFs_'+noise+str(filter)+'.dat','r')
+            stalist=station_file.read().strip().split()
+        else:
+            stalist = []
 
+        # Loop through list
+        for i in range(len(stalist)): 
+            print(i, stalist[i])
+            seis=read(stalist[i],format='PICKLE') # Read in receiver function
+            RFtrace = getattr(seis[0],filter)
+            if not 'depths' in RFtrace:
+                if seis[0].stats['dist']> 30.  and seis[0].stats['dist'] <90.:
+                    print('converting for ', stalist[i])
+                    dist=seis[0].stats['dist']                     # Set distance
+                    depth= seis[0].stats['evdp'] # set depth
+                    slat=float(seis[0].stats['stla'])
+                    slon=float(seis[0].stats['stlo'])
+                    elat= seis[0].stats['event'].origins[0]['latitude']
+                    elon= seis[0].stats['event'].origins[0]['longitude']
 
+                    RF=trace.Trace()
+                    RF.data= RFtrace['iterativedeconvolution']      # Define iterative deconvolution as a trace
 
-            if PREM:
-                # Interpolate latitude and longitudes  at the discontinuities
-                # Unfortunately obpsy taup cannot do this...yet. So this will be slow
-                discon=['P','P220s','P400s','P670s', 'P1171s']
-                x=[0,220.,400.,670., 1171.]
-            if ak135:
-                discon=['P','P210s','P410s','P660s','P1156s']
-                x=[0,210.,410.,660.,1156.]
+        
+                    # Initialize
+                    refdepths=[]
+                    reftimes=[]
+                    reftakeoff=[]
+                    refincident=[]
+                    refslow=[]
 
-            xtime=[0]
-            xlat=[slat]
-            xlon=[slon]
-            for d in range(1,len(discon)):
-                if PREM:
-                    taup_660=['taup_pierce -mod ../Tools/MODELS/PREM_FILES/prem_added_discon_taup.nd -h '+ str(depth) + ' -sta '+ str(slat)+' ' +str(slon)+ ' -evt '+ str(elat) + ' ' + str(elon) +' -ph '+discon[d] + ' -Pierce ' + str(x[d])+',0 -nodiscon']
-                if ak135:
-                    taup_660=['taup_pierce -mod ../Tools/MODELS/ak135_FILES/ak135_added_discon_taup.tvel -h '+ str(depth) + ' -sta '+ str(slat)+' ' +str(slon)+ ' -evt '+ str(elat) + ' ' + str(elon) +' -ph '+discon[d] + ' -Pierce ' + str(x[d])+',0 -nodiscon']
-                out=subprocess.check_output(taup_660,shell=True)
-                t=out.split()
-                xtime.append(float(t[-3])-float(t[-8]))
-                xlat.append(float(t[-7]))
-                xlon.append(float(t[-6]))
-
-            traveltime_poly=np.polyfit(x,xtime,4)
-            traveltime=np.poly1d(traveltime_poly)
-            lats_poly=np.polyfit(x,xlat,4)
-            lats=np.poly1d(lats_poly)
-            lons_poly=np.polyfit(x,xlon,4)
-            lons=np.poly1d(lons_poly)
+                
+                    if mod_1D == 'prem':
+                        # Get slownesses and incident angles based on PREM using Taup in Obspy
+                        phases =  ['P','Pms','P220s','P310s','P400s','P550s','P670s','P971s','P1171s']
+                        phdepths = [0,24.4,220,310,400,550,670,971,1171]
+                    if mod_1D == 'ak135':
+                        # get slownesses and incident angles based on ak135 using Taup in Obspy
+                        phases =  ['P','Pms','P210s','P410s','P660s','P1156s']
+                        phdepths = [0,35,210,410,660,1156]
 
 
-            # Convert this trace to depths and save
-            depths=np.interp(RFtrace['time'],seis[0].conversions[str(mod_1D)]['time'],seis[0].conversions[str(mod_1D)]['depths'])
-            latitudes=lats(depthspace)
-            longitudes=lons(depthspace)
+                    for p,ph in enumerate(phases):
+                        arr = taupmodel.get_travel_times(depth,dist,[ph])
+                        if p ==0:
+                            Preftime=arr[0].time
+                            Prefslow=arr[0].ray_param
+                            Preftakeoff= arr[0].takeoff_angle
+                            Prefincident = arr[0].incident_angle
+                        refdepths.append(phdepths[p])
+                        reftimes.append(arr[0].time-Preftime)
+                        reftakeoff.append(arr[0].takeoff_angle-Preftakeoff)
+                        refincident.append(arr[0].incident_angle-Prefincident)
+                        refslow.append((arr[0].ray_param-Prefslow)*np.pi/180.)
+                    depthspace=np.linspace(-100,1200,1300)
+                    # Fit polynomials
+                    takeoff_poly=np.polyfit(refdepths[1:],reftakeoff[1:],3)
+                    takeoff=np.poly1d(takeoff_poly)
+                    slow_poly=np.polyfit(refdepths,refslow,4)
+                    slowness=np.poly1d(slow_poly)
+                    time_poly=np.polyfit(refdepths,reftimes,4)
+                    timepl=np.poly1d(time_poly)
 
-            seis[0].conversions[str(mod_1D)]['depthsfortime']=depths
-            seis[0].conversions[str(mod_1D)]['latitudes']=latitudes
-            seis[0].conversions[str(mod_1D)]['longitudes']=longitudes
-            seis[0].conversions[str(mod_1D)]['latfortime']=np.interp(RFtrace['time'],seis[0].conversions[str(mod_1D)]['time'], latitudes)
-            seis[0].conversions[str(mod_1D)]['lonfortime']=np.interp(RFtrace['time'],seis[0].conversions[str(mod_1D)]['time'],longitudes)
+                    # Save solutions to dictionary
+                    if not hasattr(seis[0],'conversions'):
+                        seis[0].conversions=dict()
+                    seis[0].conversions[str(mod_1D)]=dict()
+                    seis[0].conversions[str(mod_1D)]['depths']=depthspace
+                    seis[0].conversions[str(mod_1D)]['takeoff']=takeoff(depthspace)
+                    seis[0].conversions[str(mod_1D)]['slowness']=slowness(depthspace)
+                    seis[0].conversions[str(mod_1D)]['time']=timepl(depthspace)
 
-            seis.write(stalist[i],format='PICKLE')
-          else:
-            os.remove(stalist[i])               
 
-stations=glob.glob('../Data/*')
 
-for dr in stations:
-    compute_onestation(dr,filter=rffilter)
+                    if mod_1D == 'prem':
+                        # Interpolate latitude and longitudes  at the discontinuities
+                        # Unfortunately obpsy taup cannot do this...yet. So this will be slow
+                        discon=['P','P220s','P400s','P670s', 'P1171s']
+                        x=[0,220.,400.,670., 1171.]
+                    if mod_1D == 'ak135':
+                        discon=['P','P210s','P410s','P660s','P1156s']
+                        x=[0,210.,410.,660.,1156.]
+
+                    xtime=[0]
+                    xlat=[slat]
+                    xlon=[slon]
+                    for d in range(1,len(discon)):
+                        if mod_1D == 'prem':
+                            taup_660=['taup_pierce -mod /Users/r03sb21/Documents/smurfpy/Tools/MODELS/PREM_FILES/prem_added_discon_taup.nd -h '+ str(depth) + ' -sta '+ str(slat)+' ' +str(slon)+ ' -evt '+ str(elat) + ' ' + str(elon) +' -ph '+discon[d] + ' -Pierce ' + str(x[d])+',0 -nodiscon']
+                        if mod_1D == 'ak135':
+                            taup_660=['taup_pierce -mod /Users/r03sb21/Documents/smurfpy/Tools/MODELS/ak135_FILES/ak135_added_discon_taup.tvel -h '+ str(depth) + ' -sta '+ str(slat)+' ' +str(slon)+ ' -evt '+ str(elat) + ' ' + str(elon) +' -ph '+discon[d] + ' -Pierce ' + str(x[d])+',0 -nodiscon'] #tvel
+                        out=subprocess.check_output(taup_660,shell=True)
+                        t=out.split()
+                        xtime.append(float(t[-3])-float(t[-8]))
+                        xlat.append(float(t[-7]))
+                        xlon.append(float(t[-6]))
+
+                    traveltime_poly=np.polyfit(x,xtime,4)
+                    traveltime=np.poly1d(traveltime_poly)
+                    lats_poly=np.polyfit(x,xlat,4)
+                    lats=np.poly1d(lats_poly)
+                    lons_poly=np.polyfit(x,xlon,4)
+                    lons=np.poly1d(lons_poly)
+
+
+                    # Convert this trace to depths and save
+                    depths=np.interp(RFtrace['time'],seis[0].conversions[str(mod_1D)]['time'],seis[0].conversions[str(mod_1D)]['depths'])
+                    latitudes=lats(depthspace)
+                    longitudes=lons(depthspace)
+
+                    seis[0].conversions[str(mod_1D)]['depthsfortime']=depths
+                    seis[0].conversions[str(mod_1D)]['latitudes']=latitudes
+                    seis[0].conversions[str(mod_1D)]['longitudes']=longitudes
+                    seis[0].conversions[str(mod_1D)]['latfortime']=np.interp(RFtrace['time'],seis[0].conversions[str(mod_1D)]['time'], latitudes)
+                    seis[0].conversions[str(mod_1D)]['lonfortime']=np.interp(RFtrace['time'],seis[0].conversions[str(mod_1D)]['time'],longitudes)
+
+                    seis.write(stalist[i],format='PICKLE')
+                else:
+                    os.remove(stalist[i])               
+
+    stations=glob.glob(Data+'/*')
+
+    for dr in stations:
+        compute_onestation(dr,filter=rffilter)
